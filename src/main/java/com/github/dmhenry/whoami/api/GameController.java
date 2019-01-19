5223952faf2aaad9386e09b8103a66f16642ecd7
dmhenry <>
2019-01-19 15:26:28 -0500

    Add some Swagger documentation.

--- src/main/java/com/github/dmhenry/whoami/api/GameController.java
+++ src/main/java/com/github/dmhenry/whoami/api/GameController.java
@@ -3,6 +3,8 @@ package com.github.dmhenry.whoami.api;
 import com.github.dmhenry.whoami.application.GameService;
 import com.github.dmhenry.whoami.application.model.Game;
 import com.github.dmhenry.whoami.application.model.Guess;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,12 +17,15 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1.0/games/")
+@Api(value = "Who Am I?", description = "A game to match names to faces.")
+@SuppressWarnings("unused")
 class GameController {
 
     @Autowired
     private GameService gameService;
 
     @PostMapping("standard")
+    @ApiOperation(value = "Set up a new game.", response = Game.class)
     ResponseEntity<Game> setupNewGame() {
         Game game = gameService.setupGame();
 
@@ -32,6 +37,7 @@ class GameController {
     }
 
     @GetMapping("standard/{id}")
+    @ApiOperation(value = "Get the game state.", response = Game.class)
     ResponseEntity<Game> getGame(@PathVariable String id) {
         Game game = gameService.getGame(id);
         return new ResponseEntity<>(game, new HttpHeaders(), HttpStatus.OK);
@@ -39,6 +45,7 @@ class GameController {
 
     @PostMapping("standard/{id}")
     @Valid
+    @ApiOperation(value = "Take a stab at matching the person to the image.", response = Game.class)
     ResponseEntity<Game> guess(@PathVariable String id, @RequestBody Guess guess) {
         Game game = gameService.guess(id, guess.getCandidateId());
         return new ResponseEntity<>(game, new HttpHeaders(), HttpStatus.OK);
